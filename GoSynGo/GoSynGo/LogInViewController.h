699672f84f10440d42e6709fcd0c57d7abfa5657
Intersil <intersil@Intersils-MacBook-Air.local>
2015-04-24 21:36:37 -0500

    Added files for login page

---
+++ GoSynGo/GoSynGo/LogInViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogInViewController.h
+//  GoSynGo
+//
+//  Created by Intersil on 4/24/15.
+//  Copyright (c) 2015 Pineapple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+
+@end
